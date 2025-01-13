9a5e797198a49f08ee86d2beb4eaf94882ac1b37
mgrzegor <m-grzegorczyk@o2.pl>
2025-01-13 17:09:19 +0100

    added user model

---
+++ src/Pigeon.Domain/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Pigeon.Domain;
+
+public class User
+{
+    public Guid Id { get; set; }
+    public string Username {get; set; }
+    public string Password { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
