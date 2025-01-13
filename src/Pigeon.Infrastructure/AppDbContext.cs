77a6ea9f9c746386fd999d89e6e9e652a40f7877
mgrzegor <m-grzegorczyk@o2.pl>
2025-01-13 17:09:44 +0100

    added app db context

---
+++ src/Pigeon.Infrastructure/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pigeon.Domain;
+
+namespace Pigeon.Infrastructure;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
