5d396e817df5bb89fb2efbd49259239c0aeeffd1
mgrzegor <m-grzegorczyk@o2.pl>
2025-01-14 17:27:20 +0100

    added localdb configuration

--- src/Pigeon.Infrastructure/AppDbContext.cs
+++ src/Pigeon.Infrastructure/AppDbContext.cs
@@ -5,7 +5,7 @@ namespace Pigeon.Infrastructure;
 
 public class AppDbContext : DbContext
 {
-    public AppDbContext(DbContextOptions<AppDbContext> options)
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
     {
     }
 
